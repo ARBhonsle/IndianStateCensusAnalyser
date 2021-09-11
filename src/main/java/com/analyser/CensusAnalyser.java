b9b4866b56bda975a3e9bf04586ad9bdc3bf5da7
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-11 22:24:24 +0530

    Added: Project Files

---
+++ src/main/java/com/analyser/CensusAnalyser.java
@@ -0,0 +1,12 @@
+package com.analyser;
+
+/**
+ * Indian State Census Analyser
+ */
+public class CensusAnalyser
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Indian State Census Analyser!" );
+    }
+}
